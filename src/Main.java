20cf1388a6d7834be62162088a9687c883c29d13
adil <adil.tga@outlook.fr>
2026-03-11 14:03:38 +0100

    Calcul des faiblesse et efficacite

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main{
         }catch(SQLException e){
             System.out.println("Erreur : " + e.getMessage());
         }
-        /* PokemonDAO pdao = new PokemonDAO();
+        /* PokemonDAO pdao = new PokemonDAO(); 
         Pokemon[] tabpoke;
         tabpoke = pdao.chargerTous(dbm);
         for (int i = 0; i < 151; i++) {
@@ -24,7 +24,7 @@ public class Main{
         for(int i =0; i<15 ;i++) {
             System.out.println(i+1 + ":" +  tabTypes[i].getNom());
         }
-
+    
 
     }
 
--- src/TypeDAO.java
+++ src/TypeDAO.java
@@ -5,9 +5,21 @@ import java.sql.SQLException;
 public class TypeDAO {
 
     public final static int GEN1 = 15 ;
+    private DatabaseManager dbm;
+    public TypeDAO(){
+        
+        dbm = new DatabaseManager();
+
+        try{
+            dbm.connect();
+        }catch(SQLException e){
+            System.out.println("Erreur : " + e.getMessage());
+        }
+    }
+
     public Type[] chargeType(DatabaseManager dbm){
         String sql = "SELECT * from type" ;
-        Type[] tabType = new Type[GEN1] ;
+        Type[] tabType = new Type[GEN1] ;// definir la taille du tab //
         try {
             PreparedStatement requete = dbm.getConnection().prepareStatement(sql);
             ResultSet donnee = requete.executeQuery() ;
@@ -20,9 +32,27 @@ public class TypeDAO {
         }
         catch(SQLException e)  {
             System.out.println("ERREUR DU CHARGEMENT DES TYPES : " + e.getErrorCode());
-            
-
         }
         return tabType;
+
+    }
+
+
+    public double recupMulti(int fkAtt,int fkDef ,DatabaseManager dbm){
+        String sql = "SELECT multi from efficacite WHERE fkAtt = ? and fkDef= ? LIMIT 1;";
+        try{
+
+
+        PreparedStatement pstmt = dbm.getConnection().prepareStatement(sql);
+        pstmt.setInt(1,fkAtt);
+        pstmt.setInt(2,fkDef);
+        ResultSet multi = pstmt.executeQuery(sql);
+        
+        System.out.println(multi.getDouble("multi"));
+
+        }catch(SQLException e)  {
+            System.out.println("ERREUR DU CHARGEMENT DES TYPES : " + e.getErrorCode());
+        }
+        return 1 ;
     }
 }
