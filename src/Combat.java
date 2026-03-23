48eb6ede3f47def73e8250877e57ab5c966f9025
KCShinorik <emerik.guichard@gmail.com>
2026-03-23 17:18:35 +0100

    Class Combat + Joueur

--- src/Combat.java
+++ src/Combat.java
@@ -8,24 +8,14 @@ public class Combat{
     }
 
     public Joueur victoire(Joueur joueur1, Joueur joueur2) {
-        int pokeDown1 = 0;
-        int pokeDown2 = 0;
-        for (int i = 0; i < Joueur.getTailleEquipe(); i++) {
-            if (joueur1.getEquipe()[i].getPv() == 0) {
-                pokeDown1 = pokeDown1 + 1;
-            }
-            if (joueur2.getEquipe()[i].getPv() == 0) {
-                pokeDown2 = pokeDown2 + 1;
-            }
-                if (pokeDown1 == Joueur.getTailleEquipe()) {
-                return joueur2;
-            }
-            if (pokeDown2 == Joueur.getTailleEquipe()) {
-                return joueur1;
-            }
-
+        if (joueur1.getPokemonKO() == Joueur.getTailleEquipe()) {
+            return joueur2;
+        }
+        if (joueur2.getPokemonKO() == Joueur.getTailleEquipe()) {
+            return joueur1;
         }
         return null;
+
     }
     public Joueur getJoueur1() { return joueur1;}
     public void setJoueur1(Joueur joueur1) { this.joueur1 = joueur1;}
--- src/Joueur.java
+++ src/Joueur.java
@@ -1,27 +1,29 @@
 public class Joueur{
     public static final int TAILLE_EQUIPE = 6;
-    protected String Nom;
+    protected String nom;
     protected final Pokemon[] equipe;
     protected Pokemon pokemonActif;
     protected int pokemonKO;
 
     public Joueur(String nom, Pokemon[] equipe, Pokemon pokemonActif) {
-        this.Nom = nom;
+        this.nom = nom;
         this.equipe = equipe;
         this.pokemonActif = equipe[0];
     }
 
     public Pokemon getPokemonActif() { return pokemonActif;}
+    public void setPokemonActif(Pokemon pokemonActif) {this.pokemonActif = pokemonActif;}
 
     public static int getTailleEquipe() { return TAILLE_EQUIPE;}
 
-    public String getNom() { return Nom; }
-
-    public void setNom(String nom) { Nom = nom;}
+    public String getNom() { return nom; }
+    public void setNom(String nom) {this.nom = nom;}
 
     public Pokemon[] getEquipe() {return equipe;}
 
-    public void setPokemonActif(Pokemon pokemonActif) {this.pokemonActif = pokemonActif;}
+    public int getPokemonKO() {return pokemonKO;}
+    public void setPokemonKO(int pokemonKO) {this.pokemonKO = pokemonKO;}
+    
 
 
 
