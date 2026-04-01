602a5efdb41e1929c0511694524e9a26dfda7741
adil <adil.tga@outlook.fr>
2026-04-01 13:13:47 +0200

    terk

--- src/AttaqueDAO.java
+++ src/AttaqueDAO.java
@@ -35,24 +35,4 @@ public class AttaqueDAO {
         return tabAttaque;
     }
 
-    public Attaque[] recupAttaquesAleatoires(int typeId, DatabaseManager dbm) {
-        String sql = "SELECT libelle, puissance, type_id FROM attaques WHERE type_id = ? ORDER BY RAND() LIMIT 4";
-        Attaque[] tabAttaque = new Attaque[4];
-        try {
-            PreparedStatement pstmt = dbm.getConnection().prepareStatement(sql);
-            pstmt.setInt(1, typeId);
-            ResultSet donnee = pstmt.executeQuery();
-            int i = 0;
-            while (donnee.next() && i < 4) {
-                String libelle = donnee.getString("libelle");
-                int puissance  = donnee.getInt("puissance");
-                int type       = donnee.getInt("type_id");
-                tabAttaque[i]  = new Attaque(libelle, puissance, type);
-                i++;
-            }
-        } catch (SQLException e) {
-            System.out.println("ERREUR ATTAQUES ALEATOIRES : " + e.getErrorCode());
-        }
-        return tabAttaque;
-    }
 }
